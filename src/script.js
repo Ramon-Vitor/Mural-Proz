8924b3efd5af65147a8a163b98a22841aa0a5cf7
Ramon Vitor <ramonvitorbsilva@gmail.com>
2025-02-12 22:44:28 -0300

    Posts quando clicados são abertos em tela cheia, botão para criar post quando clicado abre a area de criação em tela cheia

--- src/script.js
+++ src/script.js
@@ -1,13 +1,50 @@
-const clicarPost = document.querySelectorAll('.post');
+const clickedPost = document.querySelectorAll('.post');
 
-clicarPost.forEach(post => {
+clickedPost.forEach(post => {
     post.addEventListener('click', function() {
-        let focusPost = document.createElement("div");
-        focusPost.width = "500px";
-        focusPost.textContent = "cu";
-        focusPost.class = "post";
-        focusPost.color = "white";
-           
-        document.body.appendChild(focusPost);
+        const window = document.getElementById('windowContent');
+        const createPost = document.getElementById('createPost');
+        const btn = document.getElementById('mainButton');
+
+        createPost.classList.add('hidden');
+        window.classList.add('open');
+        btn.classList.add('hidden')
+        
+        const focusPost = document.createElement("div");
+        window.append(focusPost);
+      
+        focusPost.textContent = "Isso é um postite aberto";
+        focusPost.classList.add ('big-post');
+        focusPost.id = 'bigPost'
+        
+        
+
+        window.addEventListener('click', (e) =>{
+            if(e.target.id == 'exit' || e.target.id == 'windowContent'){
+                const bigPost = document.getElementById('bigPost')
+                window.classList.remove('open')
+                btn.classList.remove('hidden')
+                bigPost.remove()
+            };
+        });
+        
+
     })
 }) 
+
+function openWindow (){
+    const createPost = document.getElementById('createPost');
+    const window = document.getElementById('windowContent');
+    const btn = document.getElementById('mainButton');
+    
+    createPost.classList.remove('hidden');
+    window.classList.add('open');
+    btn.classList.add('hidden');
+
+    window.addEventListener('click', (e) =>{
+        if(e.target.id == 'exit' || e.target.id == 'windowContent'){
+            window.classList.remove('open')
+            btn.classList.remove('hidden')
+        };
+    });
+};
